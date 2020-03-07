ce5611106d53d698b463276f0e7521b9421688e5
Burak-Taskin <bt.taskinburak@gmail.com>
2020-03-07 16:11:44 +0300

    My Landmarbook application

---
+++ app/src/main/java/com/example/landmarkbook/DetailActivity.java
@@ -0,0 +1,34 @@
+package com.example.landmarkbook;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        ImageView imageView = findViewById(R.id.imageView);
+        TextView landmarkNameText = findViewById(R.id.landmarkNameText);
+        TextView countryNameText = findViewById(R.id.countryNameText);
+
+        Intent intent = getIntent();
+        String landmarkName = intent.getStringExtra("landmark");
+        landmarkNameText.setText(landmarkName);
+        String countryName = intent.getStringExtra("country");
+        countryNameText.setText(countryName);
+
+        Singleton singleton = Singleton.getInstance();
+        imageView.setImageBitmap(singleton.getChosenImage());
+
+        //imageView.setImageBitmap(selectedImage);
+
+    }
+}
---
+++ app/src/main/java/com/example/landmarkbook/Singleton.java
@@ -0,0 +1,27 @@
+package com.example.landmarkbook;
+
+import android.graphics.Bitmap;
+
+public class Singleton {
+
+    private Bitmap chosenImage;
+    private static Singleton singleton;
+
+    private Singleton(){}
+
+    public void setChosenImage(Bitmap chosenImage) {
+        this.chosenImage = chosenImage;
+    }
+
+    public Bitmap getChosenImage() {
+        return chosenImage;
+    }
+
+    public static Singleton getInstance(){
+        if (singleton == null) {
+            singleton = new Singleton();
+        }
+        return singleton;
+        }
+
+}
